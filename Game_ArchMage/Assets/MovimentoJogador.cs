ff9cd365f69e1d1a261edba68ef909163153fb64
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2026-05-04 23:51:12 -0300

    Upload All Archives on GitHub
    - Add -> .gitignore ->  pasta raiz
    - Configuração dos arquivos

---
+++ Game_ArchMage/Assets/MovimentoJogador.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovimentoJogador : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private  Rigidbody2D rb;
+    private Vector2 moveInput;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+       rb.velocity = moveInput * moveSpeed; 
+    }
+
+    public void Move (InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        
+        moveInput = context.ReadValue<Vector2>();   
+
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }
+}
